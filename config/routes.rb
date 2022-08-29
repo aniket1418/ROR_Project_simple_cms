53bd0011016a2539d7a8a6ce106d3660136f1188
aniket1418 <aniket.d.jain@gmail.com>
2022-08-29 20:47:46 +0530

    added migration and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  #Root Route example
+  root 'welcome#index'
+
+  #Simple route
+  get 'demo/index'
+
+  #Default Route example - May it will be removed in the future scope of versions.
+  # get ':controller(/:action(/:id))'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
